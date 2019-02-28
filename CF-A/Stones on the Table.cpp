47f5ff4c98101ea9d4728d7f9a7ff7d652a0c441
Amr Samy <36173696+Amr-Samy@users.noreply.github.com>
2019-03-01 01:39:27 +0200

    Update Stones on the Table.cpp

--- CF-A/Stones on the Table.cpp
+++ CF-A/Stones on the Table.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	int n,c=0; char a[50];
+	cin>>n;
+	for(int i=0;i<n;++i)
+	{
+		cin>>a[i];
+	}
+	for(int i=0;i<n;++i)
+	{
+		if(a[i]==a[i+1])
+		{
+			c++;
+		}
+		//else if(a[i+1]==a[i+2] && a[i]==a[i+3])
+		//{ c++; }
+	}
+	cout<<c<<endl;
+}
